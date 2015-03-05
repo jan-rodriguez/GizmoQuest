856def09f5c3acfac131f36fc760094540c99112
johnroot <johnathon.root@gmail.com>
2015-03-05 01:49:28 -0500

    First Try
    Added some assets and made a nonfunctional script

---
+++ Assets/Scripts/RightClickMoveCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightClickMoveCamera : MonoBehaviour {
+	public GameObject MainCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		MainCamera.transform.position += Vector3.right * 50;
+	}
+}
