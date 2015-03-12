bbf625a180519c88e44f2b097c567116cf6405de
jkr241 <jkr241@mit.edu>
2015-03-12 13:17:31 -0400

    Still trying to make work bench

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject dragObject;
+	Vector3 startPos;
+	CanvasGroup canvGroup;
+
+	void Start(){
+		canvGroup = GetComponent<CanvasGroup> ();
+	}
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData){
+		print ("On Begin Drag called");
+		dragObject = gameObject;
+		startPos = transform.position;
+		canvGroup.blocksRaycasts = false;
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData){
+		print ("On Drag Called");
+		transform.position = Input.mousePosition;
+	}
+
+	#endregion
+
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData) {
+		print ("On End Drag Called");
+		dragObject = null;
+		transform.position = startPos;
+		canvGroup.blocksRaycasts = true;
+	}
+
+	#endregion
+
+	
+}
