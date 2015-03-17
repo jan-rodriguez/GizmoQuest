3987e5a34d4f6cc22b200dd4cbeb6791c184d835
jkr241 <jkr241@mit.edu>
2015-03-17 01:51:20 -0400

    Can now Drag and drop into correct area and inventory is resized, really need to make code prettier

--- Assets/Scripts/DragHandler.cs
+++ Assets/Scripts/DragHandler.cs
@@ -4,21 +4,18 @@ using UnityEngine.EventSystems;
 
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-	public static GameObject dragObject;
-	Vector3 startPos;
-	CanvasGroup canvGroup;
+	Vector2 startPos;
+	Vector2 clickOffset;
 
 	void Start(){
-		canvGroup = GetComponent<CanvasGroup> ();
+
 	}
 
 	#region IBeginDragHandler implementation
 
 	public void OnBeginDrag (PointerEventData eventData){
-		print ("On Begin Drag called");
-		dragObject = gameObject;
 		startPos = transform.position;
-		canvGroup.blocksRaycasts = false;
+		clickOffset = startPos - eventData.position;
 	}
 
 	#endregion
@@ -26,23 +23,33 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 	#region IDragHandler implementation
 
 	public void OnDrag (PointerEventData eventData){
-		print ("On Drag Called");
-		transform.position = Input.mousePosition;
+		transform.position = eventData.position + clickOffset;
 	}
 
 	#endregion
 
 
-	#region IEndDragHandler implementation
+	#region IEndDropHandler implementation
 
 	public void OnEndDrag (PointerEventData eventData) {
-		print ("On End Drag Called");
-		dragObject = null;
-		transform.position = startPos;
-		canvGroup.blocksRaycasts = true;
+
+		//If they drop it in the build area, set the new parent
+		if (IsPointInRectTransform(eventData.position, WorkBenchManager.BuildArea)) {
+			transform.SetParent(WorkBenchManager.BuildArea.transform);
+		}else if (IsPointInRectTransform(eventData.position, WorkBenchManager.Inventory)){
+			//Set parent to null then actual inventory to re-position the element
+			transform.SetParent(null);
+			transform.SetParent(WorkBenchManager.Inventory.transform);
+		}
+
 	}
 
 	#endregion
 
+	static bool IsPointInRectTransform(Vector2 point, RectTransform rectTrans){
+		Vector3[] fourCornersArray = new Vector3[4];
+		rectTrans.GetWorldCorners (fourCornersArray);
+		return point.x > fourCornersArray [0].x && point.x < fourCornersArray [3].x && point.y > fourCornersArray [0].y && point.y < fourCornersArray [1].y;
+	}
 	
 }
