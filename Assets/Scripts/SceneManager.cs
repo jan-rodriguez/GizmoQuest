4dcac3f56611b09e953c1ad1a9d09a6146a40df8
jkr241 <jkr241@mit.edu>
2015-03-19 04:01:49 -0400

    Can now go to the previous level from the workbench

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager: MonoBehaviour {
+	
+	public static int previousLevel;
+	public static bool updatePreviousLevel = true;
+
+	enum Level
+	{
+		WorkBench = 0,
+		Map
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (updatePreviousLevel) {
+			previousLevel = Application.loadedLevel;
+		}
+	}
+
+	public void GoToWorkshop () {
+		// TODO: this
+		Application.LoadLevel ((int)Level.WorkBench);
+	}
+
+	public void LoadPreviousLevel () {
+		Application.LoadLevel (previousLevel);
+	}
+
+	void OnLevelWasLoaded(int level) {
+		//Only allow the previous level to be updated if we're not going to the workbench
+		if (level == (int)Level.WorkBench) {
+			updatePreviousLevel = false;
+		} else {
+			updatePreviousLevel = true;
+		}
+	}
+}
