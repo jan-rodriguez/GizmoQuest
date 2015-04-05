9da947e28690d27a30da387d00c622ee7b1c91fc
jkr241 <jkr241@mit.edu>
2015-04-05 13:52:35 -0400

    Added simple spinning animation with particle effect at end

---
+++ Assets/Scripts/ParticleHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleHandler : MonoBehaviour {
+
+	ParticleSystem emitter;
+
+	void Start() {
+		emitter = GetComponent<ParticleSystem> ();
+	}
+
+	public void StartEmitter () {
+		emitter.Play ();
+	}
+}
