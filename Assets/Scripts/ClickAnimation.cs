5469bf4c18c224bb104e808d985d5cc5a118e313
jkr241 <jkr241@mit.edu>
2015-04-29 23:25:39 -0400

    Added small animations to propeller and satellite

---
+++ Assets/Scripts/ClickAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickAnimation : MonoBehaviour {
+
+	Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animation>();
+	}
+	
+	void OnMouseDown () {
+		anim.Play();
+	}
+}
