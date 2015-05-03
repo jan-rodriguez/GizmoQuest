d886ae1fa2b0e12062065c5ac61ebbe758401556
jkr241 <jkr241@mit.edu>
2015-05-03 03:26:29 -0400

    Started working on cloud emitter stuff

---
+++ Assets/Cloud.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	private static ParticleSystem cloudEmitter;
+
+	// Use this for initialization
+	void Start () {
+
+		if (cloudEmitter == null) {
+			print ("NULL EMITTER");
+			cloudEmitter = GameObject.Find ("CloudEmitter").GetComponent<ParticleSystem>();
+		}
+
+		float randomVel = Random.Range (0, .3f);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (randomVel, 0);
+	}
+
+	void OnMouseDown () {
+		cloudEmitter.transform.parent = transform;
+		cloudEmitter.transform.localPosition = Vector2.zero;
+		cloudEmitter.Play ();
+	}
+}
