6175e919b8bcdace2226a96db7fd856b2970d3b8
jkr241 <jkr241@mit.edu>
2015-05-03 13:22:20 -0400

    Hinge joints

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,22 +4,39 @@ using System.Collections;
 public class Cloud : MonoBehaviour {
 
 	private static ParticleSystem cloudEmitter;
+	private HingeJoint2D springJoint;
 
 	// Use this for initialization
 	void Start () {
 
 		if (cloudEmitter == null) {
-			print ("NULL EMITTER");
 			cloudEmitter = GameObject.Find ("CloudEmitter").GetComponent<ParticleSystem>();
 		}
 
+		springJoint = GetComponent<HingeJoint2D>();
+
 		float randomVel = Random.Range (0, .3f);
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (randomVel, 0);
+	}
+
+	void Update() {
+		if(cloudEmitter.isPlaying){
+			if(Input.GetMouseButtonUp(0)){
+				cloudEmitter.Stop();
+			}
+		}
 	}
 
 	void OnMouseDown () {
 		cloudEmitter.transform.parent = transform;
 		cloudEmitter.transform.localPosition = Vector2.zero;
 		cloudEmitter.Play ();
+
+		if(springJoint != null){
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = transform.position.z;
+			springJoint.connectedAnchor = Camera.main.ScreenToWorldPoint(mousePos);
+			springJoint.enabled = true;
+		}
 	}
 }
