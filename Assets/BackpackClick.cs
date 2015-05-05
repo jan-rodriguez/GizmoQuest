f13236a94ce96280ecb5b6db2bd89a80641e1aa9
jkr241 <jkr241@mit.edu>
2015-05-05 02:32:11 -0400

    Added lion sleeping animation and backpack to papertowel anim. Also made clouds 100% more interactive

---
+++ Assets/BackpackClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackpackClick : MonoBehaviour {
+
+	public Sprite openSprite;
+	public GameObject child;
+	bool clicked = false;
+
+	void OnMouseDown () {
+		if (!clicked) {
+			clicked = true;
+			gameObject.GetComponent<SpriteRenderer>().sprite = openSprite;
+			child.SetActive(true);
+		}
+	}
+}
