268a114ce23ee39eeaa68bb5b986d6825bceb271
jkr241 <jkr241@mit.edu>
2015-05-13 02:26:25 -0400

    Now plays slingshot animations once the slingshot is built in the damn scene

---
+++ Assets/Scripts/SlingshotRock.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingshotRock : MonoBehaviour {
+
+	public GameObject otherRock;
+
+	void StartOtherRockAnim () {
+		otherRock.GetComponent<Animation> ().Play ();
+		Destroy (gameObject);
+	}
+}
